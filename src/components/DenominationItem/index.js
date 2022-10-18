aaafaa80a2c963e0afb17b91a8e2c5a66245179d
kowsalya89 <kowsalyachebrolu@gmail.com>
2022-10-18 07:10:48 +0000

    cash withdrawl tests

---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,21 @@
+// Write your code here
+// Write your code here
+
+import './index.css'
+
+const DenominationItem = props => {
+  const {eachItem, onClickDeduct} = props
+  const {id, value} = eachItem
+
+  const deductAmount = () => {
+    onClickDeduct(id)
+  }
+  return (
+    <li className="list-style">
+      <button className="button" type="button" onClick={deductAmount}>
+        {value}
+      </button>
+    </li>
+  )
+}
+export default DenominationItem
